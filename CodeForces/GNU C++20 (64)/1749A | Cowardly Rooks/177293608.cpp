f3968d085039c332c42c0020d4fd2b6e941f6dc3
Akshatchaube01 <92796050+Akshatchaube01@users.noreply.github.com>
2023-03-28 13:51:37 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1749A | Cowardly Rooks/177293608.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+int t;
+cin>>t;
+while(t--){
+int n,r;
+cin>>n>>r;
+for (int i = 0; i < 2*r; ++i)
+{
+    int x;
+    cin>>x;}
+if(n>r){
+    cout<<"YES"<<endl;
+}
+else{cout<<"NO"<<endl;} 
+}
+return 0;
+}
