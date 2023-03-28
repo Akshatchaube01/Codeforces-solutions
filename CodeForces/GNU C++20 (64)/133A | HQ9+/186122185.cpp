f996b6e85545e50bfcd3acb4b171dc1ebe853f3d
Akshatchaube01 <92796050+Akshatchaube01@users.noreply.github.com>
2023-03-28 13:47:42 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/133A | HQ9+/186122185.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std; 
+#define ll long long
+#define SPEED ios::sync_with_stdio(false); cin.tie(0); cout.tie(0) 
+#define fileio freopen("http://in.in", "r", stdin),freopen("out.out", "w", stdout);
+#define ld long double
+//#define M_PI (2*acos(0))
+#define MAX_N 10001
+#define MOD 1000000007
+#define apo(a,b) sqrtl( powl(a.x-b.x,2) + powl(a.y-b.y,2) )
+#define ldeq(a,b) ( fabsl(a - b) < 0.00000000007 )
+#define rep(a,b) for(int i=a; i<=b; i++)
+#define repv(a,b,i) for(int i=a; i<=b; i++)
+#define mmod(a,b) ( (a >= 0) ? ((a%b)%b) : ( ( ( a + ( ((abs(a)+b)/b) * b ) ) % b ) % b ) )
+#define int ll
+#define ALL(x) x.begin(),x.end()
+#define S(x) ((int)(x.size()))
+#define PB push_back
+void solve(){
+    string str;
+    cin>>str;
+    int count=0;
+    for(int i=0;i<str.size();i++){
+     if(str[i]=='H' || str[i]=='Q' || str[i]=='9')
+      count++;
+    }
+    if(count>0){
+     cout<<"YES"<<endl;
+    }
+    else{
+     cout<<"NO"<<endl;
+    }
+}
+#undef int
+int main(){ 
+    SPEED; 
+    cout.precision(8); 
+    cout << fixed; 
+  int t=1;
+  while(t--){
+   solve();
+  }
+    return 0; 
+} 
