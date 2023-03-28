cacf578888499475c7ecda1c13ba7e4265fca91a
Akshatchaube01 <92796050+Akshatchaube01@users.noreply.github.com>
2023-03-28 13:46:46 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/221A | Little Elephant and Function/188353477.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std; 
+#define ll long long
+#define ld long double
+#define SPEED ios::sync_with_stdio(false); cin.tie(0); cout.tie(0) 
+#define fileio freopen("http://in.in", "r", stdin),freopen("out.out", "w", stdout);
+#define rep(a,b) for(int i=a; i<=b; i++)
+#define repv(a,b,i) for(int i=a; i<=b; i++)
+#define ALL(x) x.begin(),x.end()
+#define S(x) ((int)(x.size()))
+void solve(){
+    int n;
+    cin>>n;
+    cout<<n<<" ";
+    for(int i=1;i<n;i++){
+     cout<<i<<" ";
+    }
+}
+#undef int
+int main(){ 
+    SPEED; 
+    cout.precision(8); 
+    cout << fixed;
+    solve();
+    return 0; 
+} 
