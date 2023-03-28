7101de7386d0136586d929170e13c6db83abb7ce
Akshatchaube01 <92796050+Akshatchaube01@users.noreply.github.com>
2023-03-28 13:44:15 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1551A | Polycarp and Coins/196430078.cpp
@@ -0,0 +1,34 @@
+//A. Polycarp and Coins
+#include <bits/stdc++.h>
+using namespace std; 
+#define ll long long
+#define ld long double
+#define SPEED ios::sync_with_stdio(false); cin.tie(0); cout.tie(0) 
+#define fileio freopen("http://in.in", "r", stdin),freopen("out.out", "w", stdout);
+ 
+void solve(){
+ int n;
+ cin>>n;
+ int c1=n/3;
+ int c2=n/3;
+ if(n%3==1){
+  c1++;
+ }
+ if(n%3==2){
+  c2++;
+ }
+ cout<<c1<<" "<<c2<<endl;
+}
+ 
+int main(){ 
+    SPEED; 
+    cout.precision(8); 
+    cout << fixed; 
+  int t;
+  cin>>t;
+  //t=1;
+  while(t--){
+   solve();
+  }
+    return 0; 
+} 
