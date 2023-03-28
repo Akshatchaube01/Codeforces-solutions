b722487de7063e9d9be46c4d50d0964e0af7ddf5
Akshatchaube01 <92796050+Akshatchaube01@users.noreply.github.com>
2023-03-28 13:53:05 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1061A | Coins/172375518.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std; 
+ 
+int main(){
+ ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ int n,S;
+ cin>>n>>S;
+ int ans=(n+S-1)/n;
+ cout<<ans;
+ return 0;
+}
+  
+  
